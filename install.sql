7053b9714606bb5240720c1aea1165f537c79ebc
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2017-01-20 10:03:02 +0200

    Update install.sql
    Translate comments to English

--- install.sql
+++ install.sql
@@ -1,20 +1,20 @@
 CREATE TABLE `t_task` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',
-  `group_id` int(11) NOT NULL DEFAULT '0' COMMENT '分组ID',
-  `task_name` varchar(50) NOT NULL DEFAULT '' COMMENT '任务名称',
-  `task_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '任务类型',
-  `description` varchar(200) NOT NULL DEFAULT '' COMMENT '任务描述',
-  `cron_spec` varchar(100) NOT NULL DEFAULT '' COMMENT '时间表达式',
-  `concurrent` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否只允许一个实例',
-  `command` text NOT NULL COMMENT '命令详情',
-  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0停用 1启用',
-  `notify` tinyint(4) NOT NULL DEFAULT '0' COMMENT '通知设置',
-  `notify_email` text NOT NULL COMMENT '通知人列表',
-  `timeout` smallint(6) NOT NULL DEFAULT '0' COMMENT '超时设置',
-  `execute_times` int(11) NOT NULL DEFAULT '0' COMMENT '累计执行次数',
-  `prev_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '上次执行时间',
-  `create_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `user_id` int(11) NOT NULL DEFAULT '0' COMMENT 'USER ID',
+  `group_id` int(11) NOT NULL DEFAULT '0' COMMENT 'GROUP ID',
+  `task_name` varchar(50) NOT NULL DEFAULT '' COMMENT 'TASK NAME',
+  `task_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'TASK TYPE',
+  `description` varchar(200) NOT NULL DEFAULT '' COMMENT 'DESCRIPTION',
+  `cron_spec` varchar(100) NOT NULL DEFAULT '' COMMENT 'CRON SPEC',
+  `concurrent` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'CONCURRENT',
+  `command` text NOT NULL COMMENT 'COMMAND',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'STATUS',
+  `notify` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'NOTIFY',
+  `notify_email` text NOT NULL COMMENT 'NOTIFY EMAIL',
+  `timeout` smallint(6) NOT NULL DEFAULT '0' COMMENT 'TIMEOUT',
+  `execute_times` int(11) NOT NULL DEFAULT '0' COMMENT 'EXECUTE TIMES',
+  `prev_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'PREVIOUS TIME',
+  `create_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'CREATE TIME',
   PRIMARY KEY (`id`),
   KEY `idx_user_id` (`user_id`),
   KEY `idx_group_id` (`group_id`)
@@ -22,10 +22,10 @@ CREATE TABLE `t_task` (
 
 CREATE TABLE `t_task_group` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',
-  `group_name` varchar(50) NOT NULL DEFAULT '' COMMENT '组名',
-  `description` varchar(255) NOT NULL DEFAULT '' COMMENT '说明',
-  `create_time` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `user_id` int(11) NOT NULL DEFAULT '0' COMMENT 'USERID',
+  `group_name` varchar(50) NOT NULL DEFAULT '' COMMENT 'GROUP NAME',
+  `description` varchar(255) NOT NULL DEFAULT '' COMMENT 'DESCRIPTION',
+  `create_time` int(11) NOT NULL DEFAULT '0' COMMENT 'CREATE TIME',
   PRIMARY KEY (`id`),
   KEY `idx_user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -33,12 +33,12 @@ CREATE TABLE `t_task_group` (
 
 CREATE TABLE `t_task_log` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `task_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '任务ID',
-  `output` mediumtext NOT NULL COMMENT '任务输出',
-  `error` text NOT NULL COMMENT '错误信息',
-  `status` tinyint(4) NOT NULL COMMENT '状态',
-  `process_time` int(11) NOT NULL DEFAULT '0' COMMENT '消耗时间/毫秒',
-  `create_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `task_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'TASK ID',
+  `output` mediumtext NOT NULL COMMENT 'OUTPUT',
+  `error` text NOT NULL COMMENT 'ERROR',
+  `status` tinyint(4) NOT NULL COMMENT 'STATUS',
+  `process_time` int(11) NOT NULL DEFAULT '0' COMMENT 'PROCESS TIME',
+  `create_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'CREATE TIME',
   PRIMARY KEY (`id`),
   KEY `idx_task_id` (`task_id`,`create_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -46,13 +46,13 @@ CREATE TABLE `t_task_log` (
 
 CREATE TABLE `t_user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(20) NOT NULL DEFAULT '' COMMENT '用户名',
-  `email` varchar(50) NOT NULL DEFAULT '' COMMENT '邮箱',
-  `password` char(32) NOT NULL DEFAULT '' COMMENT '密码',
-  `salt` char(10) NOT NULL DEFAULT '' COMMENT '密码盐',
-  `last_login` int(11) NOT NULL DEFAULT '0' COMMENT '最后登录时间',
-  `last_ip` char(15) NOT NULL DEFAULT '' COMMENT '最后登录IP',
-  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态，0正常 -1禁用',
+  `user_name` varchar(20) NOT NULL DEFAULT '' COMMENT 'USER NAME',
+  `email` varchar(50) NOT NULL DEFAULT '' COMMENT 'EMAIL',
+  `password` char(32) NOT NULL DEFAULT '' COMMENT 'PASSWORD',
+  `salt` char(10) NOT NULL DEFAULT '' COMMENT 'SALT',
+  `last_login` int(11) NOT NULL DEFAULT '0' COMMENT 'LAST LOGIN',
+  `last_ip` char(15) NOT NULL DEFAULT '' COMMENT 'LAST IP',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'STATUS，0=ENABLED -1=DISABLED',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_user_name` (`user_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
