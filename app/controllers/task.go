ae0e24ff38937d09759bde160a984f6d1e68a85a
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2017-01-21 13:52:44 +0200

    Update import statements

--- app/controllers/task.go
+++ app/controllers/task.go
@@ -3,9 +3,9 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 	libcron "github.com/lisijie/cron"
-	"github.com/lisijie/webcron/app/jobs"
-	"github.com/lisijie/webcron/app/libs"
-	"github.com/lisijie/webcron/app/models"
+	"github.com/anselal/webcron/app/jobs"
+	"github.com/anselal/webcron/app/libs"
+	"github.com/anselal/webcron/app/models"
 	"strconv"
 	"strings"
 	"time"
