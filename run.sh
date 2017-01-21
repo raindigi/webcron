169a99b9c148c01fc888b9fbc78fd72ffd320252
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2017-01-21 13:47:46 +0200

    Translate String to English

--- run.sh
+++ run.sh
@@ -3,19 +3,19 @@
 case $1 in 
 	start)
 		nohup ./webcron 2>&1 >> info.log 2>&1 /dev/null &
-		echo "服务已启动..."
+		echo "Service started..."
 		sleep 1
 	;;
 	stop)
 		killall webcron
-		echo "服务已停止..."
+		echo "Service stopped..."
 		sleep 1
 	;;
 	restart)
 		killall webcron
 		sleep 1
 		nohup ./webcron 2>&1 >> info.log 2>&1 /dev/null &
-		echo "服务已重启..."
+		echo "Service restarted..."
 		sleep 1
 	;;
 	*) 
