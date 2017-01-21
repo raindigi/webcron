68382adede8881f5c1f3b3e2468854acfdab90ee
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2017-01-21 13:51:56 +0200

    Update import statements

--- app/controllers/group.go
+++ app/controllers/group.go
@@ -2,8 +2,8 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/lisijie/webcron/app/libs"
-	"github.com/lisijie/webcron/app/models"
+	"github.com/anselal/webcron/app/libs"
+	"github.com/anselal/webcron/app/models"
 	"strconv"
 	"strings"
 )
